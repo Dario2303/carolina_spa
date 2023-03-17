201dbca08dbc941f2f5ad21ad53975e6cb42119e
dariorod <dario2303@gmail.com>
2023-03-17 11:40:30 -0300

    finished project

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ window.swiper = new Swiper({
     slideClass: 'slider__slide',
     createElements: true,
     autoplay: {
-        delay: 5000
+        delay: 2000
     },
     loop: true,
+    speed: 1000,
     spaceBetween: 100,
     pagination: true,
     navigation: true,
